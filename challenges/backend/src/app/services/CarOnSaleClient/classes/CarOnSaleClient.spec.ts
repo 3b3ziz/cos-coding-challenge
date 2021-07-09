0ef4126f90f12ef995c75e421b073c7c2523cc2f
Ahmad Abdelaziz <a.3b3ziz@gmail.com>
2021-07-10 01:33:58 +0200

    feat: implement the actual CarOnSale client and update tests accordingly

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
@@ -1,12 +1,14 @@
 import { expect } from 'chai';
-import { ICarOnSaleClient } from "../interface/ICarOnSaleClient";
-import { CarOnSaleClient } from "./CarOnSaleClient"
+import { ICarOnSaleClient } from '../interface/ICarOnSaleClient';
+import { CarOnSaleClient } from './CarOnSaleClient';
 
-
-describe('CarOnSaleClient getRunningAuctions', function() {
-    it('should return an array of auction items', async function() {
-        const carOnSaleClient: ICarOnSaleClient = new CarOnSaleClient()
-        const runningAuctions = await carOnSaleClient.getRunningAuctions()
-        expect(runningAuctions).to.be.an('array')
-    });
+describe('CarOnSaleClient getRunningAuctions', function () {
+  it('should return an array of auction items', async function () {
+    const carOnSaleClient: ICarOnSaleClient = new CarOnSaleClient();
+    const { data: runningAuctions } = await carOnSaleClient.getRunningAuctions();
+    expect(runningAuctions).to.be.an('object');
+    expect(runningAuctions).to.have.own.property('items');
+    expect(runningAuctions).to.have.own.property('page');
+    expect(runningAuctions).to.have.own.property('total');
+  });
 });
--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -1,14 +1,39 @@
+import axios, { AxiosResponse } from 'axios';
 import { injectable } from 'inversify';
 import 'reflect-metadata';
 import { ICarOnSaleClient } from '../interface/ICarOnSaleClient';
+
+const BASE_URL = 'https://api-core-dev.caronsale.de/api';
+const CREATE_AUTH_TOKEN_ENDPOINT = (email: string) => `/v1/authentication/${email}`;
+const LIST_RUNNING_AUCTIONS_ENDPOINT = () => `/v2/auction/buyer/`;
+
+const EMAIL = 'salesman@random.com';
+const PASSWORD = '123test';
+
+axios.defaults.baseURL = BASE_URL;
 
 @injectable()
 export class CarOnSaleClient implements ICarOnSaleClient {
   public constructor() {}
 
-  public async getRunningAuctions(): Promise<any> {
-    return new Promise((resolve, _) => {
-        resolve([]);
-    });
+  private async createAuthToken(email: string, password: string): Promise<AxiosResponse<any>> {
+    return axios.put(CREATE_AUTH_TOKEN_ENDPOINT(email), { password });
+  }
+
+  public async getRunningAuctions(): Promise<AxiosResponse<any>> {
+    try {
+      const { data: authResult } = await this.createAuthToken(EMAIL, PASSWORD);
+      const { token, userId } = authResult;
+
+      return axios.get(LIST_RUNNING_AUCTIONS_ENDPOINT(), {
+        headers: {
+          authtoken: token,
+          userid: userId
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
